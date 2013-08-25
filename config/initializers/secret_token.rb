fd39b15614023db3a099bd0394fb98799df22ae8
Tomás Azevedo <tomasaazevedo@gmail.com>
2013-08-25 20:09:58 -0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrisazevedoWebsite::Application.config.secret_key_base = 'bb4fb2fa328e0c43a92ed506f400e3d8834dba3927f2b75239d8cafb1228aba3fb8ff3267464b641f05b754add17b00e32d234535b0c4f791f690a99337c5c92'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CrisazevedoWebsite::Application.config.session_store :cookie_store, key: '_crisazevedo_website_session'
