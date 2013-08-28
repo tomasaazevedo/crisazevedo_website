e6aee2c2f6d0708e9c13148fc57ea8137dfaeb6b
Tomás Azevedo <tomasaazevedo@gmail.com>
2013-08-28 08:26:17 -0300

    Added Users controller + bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 CrisazevedoWebsite::Application.routes.draw do
+  get "users/new"
+  root 'index#home'
+  match '/signup',  to: 'users#new',            via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
